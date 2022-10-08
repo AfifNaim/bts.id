c77e5e421624a6e015fb09d577f3614bf22346a9
afifnaim24 <afifnaim24@gmail.com>
2022-10-08 16:36:08 +0700

    create API Checklist App

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'username'  => 'required|unique:users',
+            'email'     => 'required|email|unique:users',
+            'password'  => 'required|min:8'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $user = User::create([
+            'username'  => $request->username,
+            'email'  => $request->email,
+            'password'  => bcrypt($request->password)
+        ]);
+
+        if($user) {
+            return response()->json([
+                'success' => true,
+                'user'    => $user,  
+            ], 201);
+        }
+
+        return response()->json([
+            'success' => false,
+        ], 409);
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'username'     => 'required',
+            'password'  => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $credentials = $request->only('username', 'password');
+
+        if(!$token = auth()->guard('api')->attempt($credentials)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Username atau Password Anda salah'
+            ], 401);
+        }
+
+        return response()->json([
+            'success' => true,
+            'user'    => auth()->guard('api')->user(),    
+            'token'   => $token   
+        ], 200);
+    }
+}
